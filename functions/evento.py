d093f34466e3001aa0860ab543b28e70233da553
luccabibar <luccabibar@gmail.com>
2024-11-16 01:56:47 -0300

    evento agora retorna todas as informacoes

--- functions/evento.py
+++ functions/evento.py
@@ -16,25 +16,33 @@ def evento(dbconf, id):
         return 500, "impossivel conectar com o banco"
 
     # monta request
-    colnames = ['nome', 'descricao', 'categoria', 'horarioInicio', 'horarioFim', 'regiao', 'endereco', 'enderecoLink', 'organizador']
+    colnames = [
+        'nome', 'descricao', 'categoria', 'contato',
+        'horarioInicio', 'horarioFim', 'regiao', 'endereco', 'enderecoLink', 
+        'organizador', 
+        'attTitulo', 'attTexto'
+    ]
 
     query = '''
         SELECT
-            ev.nome, ev.descricao, ev.categoria,
-            ev.hora_ini, ev.hora_fim,
-            ev.regiao, ev.endereco, ev.endereco_link,
-            us.nome
+            ev.nome, ev.descricao, ev.categoria, ev.contato,
+            ev.hora_ini, ev.hora_fim, ev.regiao, ev.endereco, ev.endereco_link,
+            us.nome,
+            att.titulo, att.texto
         FROM evento AS ev
         JOIN organizador AS og
         ON
             ev.organizador = og.id
         JOIN usuario AS us
         ON
-             og.id = us.id
+            og.id = us.id
+        LEFT JOIN atualizacao_evento AS att
+		ON
+			ev.id = att.evento
         WHERE
             ev.id = %s
             AND ev.status = 'Aprovado'
-        LIMIT 1;
+        ;
     '''
 
     params = (id,)
@@ -48,6 +56,26 @@ def evento(dbconf, id):
         return 404, "Nenhum evento encontrado"
     
     # pos processamento
+    result[0]['atualizacoes'] = []
+    
     result[0]['organizador'] = { 'nome': result[0]['organizador'] } 
 
+    if result[0]['attTexto'] is not None:
+        
+        for row in result:
+            result[0]['atualizacoes'].append({
+                'titulo': row['attTitulo'],
+                'texto': row['attTexto']
+            })
+
+        del result[0]['attTitulo']
+        del result[0]['attTexto']
+
     return 200, result[0]
+
+
+def getEvento():
+    pass
+
+def getAtualizacoes():
+    pass
