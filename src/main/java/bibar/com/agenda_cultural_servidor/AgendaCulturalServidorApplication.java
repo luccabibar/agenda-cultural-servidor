a21b36ade99dcd1af683f02c4824df50c9705e90
luccabibar <luccabibar@gmail.com>
2025-07-01 19:41:03 -0300

    'initial' commit

---
+++ src/main/java/bibar/com/agenda_cultural_servidor/AgendaCulturalServidorApplication.java
@@ -0,0 +1,13 @@
+package bibar.com.agenda_cultural_servidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaCulturalServidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaCulturalServidorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bibar/com/agenda_cultural_servidor/AgendaCulturalServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package bibar.com.agenda_cultural_servidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaCulturalServidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
