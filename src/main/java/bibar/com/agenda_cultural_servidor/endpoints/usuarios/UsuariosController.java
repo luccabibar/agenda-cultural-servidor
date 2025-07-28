eab115bbc2c279f38cc0197b399727774ee04125
luccabibar <luccabibar@gmail.com>
2025-07-28 20:22:57 -0300

    verificacao usuario existe

--- src/main/java/bibar/com/agenda_cultural_servidor/endpoints/usuarios/UsuariosController.java
+++ src/main/java/bibar/com/agenda_cultural_servidor/endpoints/usuarios/UsuariosController.java
@@ -1,8 +1,13 @@
 package bibar.com.agenda_cultural_servidor.endpoints.usuarios;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import bibar.com.agenda_cultural_servidor.records.ResponseWrapper;
 
 @RestController
 @CrossOrigin
--- src/main/java/bibar/com/agenda_cultural_servidor/endpoints/usuarios/UsuariosRepository.java
+++ src/main/java/bibar/com/agenda_cultural_servidor/endpoints/usuarios/UsuariosRepository.java
@@ -1,5 +1,8 @@
 package bibar.com.agenda_cultural_servidor.endpoints.usuarios;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +15,37 @@ public class UsuariosRepository
         JdbcClient jdbcClientInj
     ) {
         jdbcClient = jdbcClientInj;
+    }
+
+    public boolean usuarioExiste (String nome, String email, String cpf)
+    {
+        String query = """
+            SELECT
+                us.email, 
+                us.nome, 
+                og.cpf_cnpj, 
+                md.cpf_cnpj 
+            FROM usuario AS us
+            FULL OUTER JOIN organizador AS og
+                ON us.id = og.id
+            FULL OUTER JOIN moderador AS md
+                ON us.id = md.id
+            WHERE
+                UPPER(us.email) = UPPER(:email)
+                OR UPPER(us.nome) = UPPER(:nome)
+                OR og.cpf_cnpj = :cpf
+                OR md.cpf_cnpj = :cpf
+            ;
+        """;
+
+        List<Map<String, Object>> resList = jdbcClient
+            .sql(query)
+            .param("email", email)
+            .param("nome", nome)
+            .param("cpf", cpf)
+            .query()
+            .listOfRows();
+
+        return resList.size() > 0;
     }
 }
