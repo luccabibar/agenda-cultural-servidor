dbe3da3779c4b9a41bd73f6d544ca779fa96e029
luccabibar <luccabibar@gmail.com>
2025-07-31 00:00:40 -0300

    adicionado validacao via anotation para classe tipo request body

--- src/main/java/bibar/com/agenda_cultural_servidor/endpoints/usuarios/UsuariosController.java
+++ src/main/java/bibar/com/agenda_cultural_servidor/endpoints/usuarios/UsuariosController.java
@@ -1,7 +1,5 @@
 package bibar.com.agenda_cultural_servidor.endpoints.usuarios;
 
-import java.util.Map;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import bibar.com.agenda_cultural_servidor.records.ResponseWrapper;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 
 @RestController
 @CrossOrigin
@@ -25,57 +25,14 @@ public class UsuariosController
     }
 
 
-    // original
-    // @PostMapping("/pessoas")
-    // public ResponseEntity<ResponseWrapper<Boolean>> criaPessoa(
-    //     @RequestBody String nome,
-    //     @RequestBody String email,
-    //     @RequestBody String senha
-    // ) {
-    //     System.out.println(nome + "\n" + email + "\n" + senha + "\n");
-
-    //     boolean result = usuariosService.criaPessoa(nome, email, senha);
-
-    //     ResponseWrapper<Boolean> response = new ResponseWrapper<Boolean>(result);
-    //     return ResponseEntity.ok(response);
-    // }
-
-    // alternativa A - Map
-    // @PostMapping("/pessoas")
-    // public ResponseEntity<ResponseWrapper<Boolean>> criaPessoa(
-    //     @RequestBody Map<String, Object> body
-    // ) {
-    //     String nome, email, senha;
-
-    //     try {
-            
-    //         nome = (String) body.get("nome");
-    //         email = (String) body.get("email");
-    //         senha = (String) body.get("senha");
-    //     }
-    //     catch(Exception ex) {
-    //         return ResponseEntity.badRequest().build();
-    //     }
-        
-    //     boolean result = usuariosService.criaPessoa(nome, email, senha);
-
-    //     ResponseWrapper<Boolean> response = new ResponseWrapper<Boolean>(result);
-    //     return ResponseEntity.ok(response);
-    // }
-
     @PostMapping("/pessoas")
     public ResponseEntity<ResponseWrapper<Boolean>> criaPessoa(
-        @RequestBody CriaPessoaRequestBody body
+        @Valid @RequestBody CriaPessoaRequestBody body
     ) {
-        if(body.isValid())
-            return ResponseEntity.badRequest().build();
-
         String nome = body.nome();
         String email = body.email();
         String senha = body.senha();
-        
-        System.out.println(nome + "\n" + email + "\n" + senha + "\n");
-
+       
         boolean result = usuariosService.criaPessoa(nome, email, senha);
 
         ResponseWrapper<Boolean> response = new ResponseWrapper<Boolean>(result);
@@ -83,14 +40,8 @@ public class UsuariosController
     }
 }
 
-record CriaPessoaRequestBody (String nome, String email, String senha)
-{
-    // cheque se valores nao requeridos nao esao sendo verificados
-    public boolean isValid()
-    {
-        return
-            nome == null || 
-            email == null ||
-            senha == null;
-    }
-}
+record CriaPessoaRequestBody (
+    @NotBlank String nome, 
+    @NotBlank String email, 
+    @NotBlank String senha
+) { }
